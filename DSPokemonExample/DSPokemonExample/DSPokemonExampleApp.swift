48705b15b64e5cc917b58c5d0d994253e96aa190
Luis Gustavo <luisgustavob1@outlook.com>
2024-02-12 20:56:43 +0200

    Initial commit

---
+++ DSPokemonExample/DSPokemonExample/DSPokemonExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSPokemonExampleApp.swift
+//  DSPokemonExample
+//
+//  Created by Luis Gustavo on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DSPokemonExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
